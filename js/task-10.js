5ae2abdb913e394ed71699e0a768395e94fb177b
Sergey Shishkin <Shishkin_SU@ukr.net>
2022-07-09 17:48:53 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const amountContainer = document.querySelector('#controls');
+
+console.log(amountContainer);
+
+const createBtn = amountContainer.querySelector('[data-create]');
+const destroyBtn = amountContainer.querySelector('[data-destroy]');
+
+const inputNumb = amountContainer.querySelector('input[type="number"]');
+
+console.log(inputNumb);
+
+const boxesMarkup = document.querySelector('#boxes');
+let boxWidth = 30;
+let boxHeight = 30;
+
+function createBoxes(amount) {
+  let markupArray = [];
+
+  for (
+    let i = Number(inputNumb.min);
+    i <= amount;
+    i += Number(inputNumb.step)
+  ) {
+    let boxMarkup = `<div style="width:${boxWidth}px; height:${boxHeight}px; background-color:${getRandomHexColor()}"></div>`;
+
+    boxWidth += 10;
+    boxHeight += 10;
+    markupArray.push(boxMarkup);
+  }
+  boxesMarkup.insertAdjacentHTML('beforeend', markupArray.join(''));
+}
+destroyBtn.addEventListener('click', () => {
+  boxesMarkup.remove();
+  inputNumb.value = 0;
+});
+createBtn.addEventListener('click', () => createBoxes(inputNumb.value));
